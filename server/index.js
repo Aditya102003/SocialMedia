021ec6b4c5129cb85c97d369e9264b16993eafd5
unknown <apsharma202003@gmail.com>
2024-04-08 02:22:21 +0530

    c

--- server/index.js
+++ server/index.js
@@ -33,7 +33,7 @@ app.use(morgan("common"));
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors(
-  {origin: ['https://social-media-seven-theta.vercel.app/']}
+  {origin: ['https://social-media-seven-theta.vercel.app/',"http://localhost:3000/"],}
 ));
 app.use("/assets", express.static(path.join(__dirname, "public/assets")));
 
