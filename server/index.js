d149e30aeffaf49e92f0b06d9e52d5d16c4879a4
unknown <apsharma202003@gmail.com>
2024-04-08 00:53:48 +0530

    made some new changes 1

--- server/index.js
+++ server/index.js
@@ -17,6 +17,9 @@ import { verifyToken } from "./middleware/auth.js";
 import User from "./models/User.js";
 import Post from "./models/Post.js";
 import { users, posts } from "./data/index.js";
+import env from "dotenv"
+
+env.config();
 
 /* CONFIGURATIONS */
 const __filename = fileURLToPath(import.meta.url);
@@ -62,8 +65,5 @@ mongoose
   .then(() => {
     app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
 
-    /* ADD DATA ONE TIME */
-    // User.insertMany(users);
-    // Post.insertMany(posts);
   })
   .catch((error) => console.log(`${error} did not connect`));
