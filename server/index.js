ffb5269bb495fc80bb9d0587074e6ce220735349
unknown <apsharma202003@gmail.com>
2024-04-08 02:33:36 +0530

    reoved

--- server/index.js
+++ server/index.js
@@ -32,9 +32,9 @@ app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common"));
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
-app.use(cors(
-  {origin: ['https://social-media-seven-theta.vercel.app/',"http://localhost:3000/"],}
-));
+// app.use(cors(
+//   {origin: ['https://social-media-seven-theta.vercel.app/',"http://localhost:3000/"],}
+// ));
 app.use("/assets", express.static(path.join(__dirname, "public/assets")));
 
 /* FILE STORAGE */
