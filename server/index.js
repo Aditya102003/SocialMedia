4b740ef2009552603b3f5b1cd171fc9b1cbb7c4e
unknown <apsharma202003@gmail.com>
2024-04-08 01:14:07 +0530

    added cors

--- server/index.js
+++ server/index.js
@@ -32,7 +32,9 @@ app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common"));
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
-app.use(cors());
+app.use(cors(
+  {origin: ['https://social-media-seven-theta.vercel.app/']}
+));
 app.use("/assets", express.static(path.join(__dirname, "public/assets")));
 
 /* FILE STORAGE */
